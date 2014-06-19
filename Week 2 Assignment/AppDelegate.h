e05c1fb30effc39d33a2ea841e546c21fa2d3cd7
Billy Roh <billy.roh@gmail.com>
2014-06-18 17:04:07 -0700

    Initial Commit

---
+++ Week 2 Assignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week 2 Assignment
+//
+//  Created by Billy Roh on 6/18/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week 2 AssignmentTests/Week_2_AssignmentTests.m
@@ -0,0 +1,34 @@
+//
+//  Week_2_AssignmentTests.m
+//  Week 2 AssignmentTests
+//
+//  Created by Billy Roh on 6/18/14.
+//  Copyright (c) 2014 Billy Roh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week_2_AssignmentTests : XCTestCase
+
+@end
+
+@implementation Week_2_AssignmentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
